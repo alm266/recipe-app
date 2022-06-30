044b079cbf4c6934a5d46d230faccfc805e675be
Andrew Marshall <andrewleonmarshall@gmail.com>
2022-06-30 11:07:39 -0400

    Created components listing and deleting recipes

--- my-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2022%2F04%2F15%2FSour-Power-FT-2-MAG0522.jpg&w=1100&h=737&c=sc&poi=face&q=60'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit() {}
 }
